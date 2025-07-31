542625d1ed70f63dc5efca3e47c71c48bb2c07d9
saiharsha61 <121154104+saiharsha61@users.noreply.github.com>
2025-07-31 11:33:03 +0530

    Create Blog.js

---
+++ Blog.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Blog() {
+  return (
+    <section className="blog">
+      <h2>Blog</h2>
+      <p>Coming soon: Articles on AI, Automation, and Cybersecurity!</p>
+    </section>
+  );
+}
+
+export default Blog;
